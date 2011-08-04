33db11d4331aa57b71daaae2003a33404ed47f38
Josh Daghlian <josh@daghlian.net>
2011-08-04 00:59:49 -0400

    add/drop domains, add/drop columns

--- src/grotte/core.clj
+++ src/grotte/core.clj
@@ -1,1 +1,43 @@
-(ns grotte.core)
+(ns grotte.core
+  (:import java.util.UUID))
+
+(defonce *domains* (ref {}))
+(defonce *columns* (ref {}))
+(defonce *rows*    (ref {}))
+
+;;;;;;;;;;;;;;
+;; Create and destroy (hide) domains (classes)
+;;
+(defn create-domain
+  [^Keyword domain]
+  (dosync
+   (alter *domains* assoc domain :visible)
+   (let [old-columns-ref (get @*columns* domain)]
+     (if (nil? old-columns-ref)
+       (do (alter *columns* assoc domain (ref []))
+           (alter *rows*    assoc domain (ref [])))))))
+
+(defn drop-domain
+  [^Keyword domain]
+  (dosync (alter *domains* assoc domain :hidden)))
+;;
+;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;
+;; Add and remove columns
+;;
+(defn add-column
+  [^Keyword domain ^Keyword column]
+  (dosync (alter (get @*columns* domain) conj column)))
+
+(defn drop-column
+  [^Keyword domain ^Keyword column]
+  (dosync (alter (get @*columns* domain) (partial remove #(= column %)))))
+;;
+;;;;;;;;;;;;;;
+
+
+;(defn add-row)
+;(defn update-row)
+;(defn delete-row)
