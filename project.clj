6716064c58afd6e4f40ae6784634ef19e30b092e
Josh Daghlian <josh@daghlian.net>
2011-08-03 23:48:15 -0400

    lein new grotte

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject grotte "0.0.1-SNAPSHOT"
+  :description "Grotte: Edit-in-place crud schemas and data."
+  :dependencies [[org.clojure/clojure "1.2.1"]])
---
+++ src/grotte/core.clj
@@ -0,0 +1,1 @@
+(ns grotte.core)
