174a3c8ff5d24bf01b91e7e4380d0acd6af1c1fb
Josh Daghlian <josh@daghlian.net>
2015-03-29 15:46:54 -0400

    update dependency versions

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject grotte "0.0.1-SNAPSHOT"
   :description "Grotte: Edit-in-place crud schemas and data."
 ;;  :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=4321"]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 ;[org.clojure/clojure-contrib "1."]
-                 [org.clojure/data.json "0.2.3"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.5"]
                  [ring "1.3.2"]
                  [compojure "1.3.2"]
                  [hiccup "1.0.5"]
